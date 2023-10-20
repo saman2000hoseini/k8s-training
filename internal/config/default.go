836f7b450260072b52425b2df933873c8f337339
Sam <saman2000hoseini@gmail.com>
2023-10-21 02:15:41 +0330

    feat: add full docker-compose version

--- internal/config/default.go
+++ internal/config/default.go
@@ -5,7 +5,7 @@ import "github.com/saman2000hoseini/k8s-training/internal/db"
 func Default() Config {
 	return Config{
 		Redis: db.Config{
-			Address:  "localhost:6379",
+			Address:  "redis:6379",
 			Password: "secret",
 		},
 		Server: Server{Port: 65432},
